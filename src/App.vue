dd22646bf0943c467f6e8d90c6a4c093ab18d64b
nazeliaslanyan <nazeli.aslanyan7@gmail.com>
2024-03-30 21:38:32 +0400

    Added styless

--- src/App.vue
+++ src/App.vue
@@ -32,42 +32,21 @@ export default {
 <template>
   <div>
     
-    <button @click="checkEmail()">Input Email</button>
+    <button @click="checkEmail">Input Email</button>
 
-    <h1></h1> 
+    <h1 >{{ email }}</h1>
     <h4 v-if="emailIsValid">The email is valid</h4>
     <h4 v-else>The email is invalid</h4>
 
     
-    <button @click="resetEmail()">Reset</button>
+    <button @click="resetEmail">Reset</button>
   </div>
 </template>
 
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+h1 {
+  border: 2px solid;
+  padding: 5px;
 }
 </style>
