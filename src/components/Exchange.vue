7623a37c3db7efd1aeaabd6a43432433b3491f61
nazeliaslanyan <nazeli.aslanyan7@gmail.com>
2024-04-01 20:05:17 +0400

    Added Exchange component?

---
+++ src/components/Exchange.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  data: function() {
+    return {
+        exchange:{},
+      
+    };
+  },
+  methods: {
+    
+   
+    },
+    
+   
+}
+</script>
+
+<template>
+<div>
+    <input 
+      type="number"
+      v-model="enterAmount"
+      @input="convertCurrency"
+    >
+    <input 
+      type="number"
+      v-model="convertAmount"
+      @input="handleInputChange"
+    >
+    <button @click="toggleMode">Toggle Mode</button>
+  </div>
+  
+</template>
+
+
+<style scoped>
+
+</style>
