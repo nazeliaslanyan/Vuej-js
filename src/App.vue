e3260c1ac22e8bd3f00998b6dfa43ea93e8de563
nazeliaslanyan <nazeli.aslanyan7@gmail.com>
2024-04-01 21:11:29 +0400

    Added changed Exchange component

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,22 @@
 <script>
+import Exchange from './components/Exchange.vue';
+
 export default {
-  data: function() {
-    return {
-      email: '', 
-      emailIsValid: false 
-    };
+  components: {
+    Exchange
   },
-  methods: {
-    
-    checkEmail() {
-      const inputEmail = prompt("Please enter your email:");
-      if (inputEmail !== null) {
-        this.email = inputEmail.trim();
-        this.emailIsValid = this.validateEmail(this.email);
-      }
-    },
-    
-    validateEmail(email) {
-      const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      return regex.test(email);
-    },
-    
-    resetEmail() {
-      this.email = '';
-      this.emailIsValid = false;
-    }
-  }
+ 
 }
 </script>
 
 <template>
-  <div>
-    
-    <button @click="checkEmail">Input Email</button>
+  <main>
+    <Exchange />
 
-    <h1 :style="{ borderColor: emailIsValid ? 'green' : 'red' }">{{ email }}</h1>
-    <h4 v-if="emailIsValid">The email is valid</h4>
-    <h4 v-else>The email is invalid</h4>
-
-    
-    <button @click="resetEmail">Reset</button>
-  </div>
+  </main>
 </template>
 
 
 <style scoped>
-h1 {
-  border: 2px solid;
-  padding: 5px;
-}
+
 </style>
